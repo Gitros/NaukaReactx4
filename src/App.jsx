5ead4dbab201a41a20de19647625be5957343a55
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-03-05 13:03:53 +0100

    investment app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import InvestmentResults from "./components/InvestmentResults";
+import UserInput from "./components/UserInput";
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIndentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIndentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} handleChange={handleChange} />
+      {!inputIsValid && <p className="center"> enter valid number</p>}
+      {inputIsValid && <InvestmentResults userInput={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
